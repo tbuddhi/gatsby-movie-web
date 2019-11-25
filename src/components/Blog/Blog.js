445b7a350b0f8dea5f6b8e367683e802b024bdbe
buddhi.r <buddhi.r@evicio.com>
2019-11-25 22:02:41 +0530

    upload files

---
+++ src/components/Blog/Blog.js
@@ -0,0 +1,54 @@
+import PropTypes from "prop-types";
+import React from "react";
+
+import Item from "./Item";
+
+const Blog = props => {
+  const { posts, theme } = props;
+
+  return (
+    <React.Fragment>
+      <main className="main">
+        <ul>
+          {posts.map(post => {
+            const {
+              node,
+              node: {
+                fields: { slug }
+              }
+            } = post;
+            return <Item key={slug} post={node} theme={theme} />;
+          })}
+        </ul>
+      </main>
+
+      {/* --- STYLES --- */}
+      <style jsx>{`
+        .main {
+          // background-color: ${theme.color.neutral.black};
+        }
+
+        ul {
+          list-style: none;
+          display: flex;
+          flex-wrap: wrap;
+          padding: 20px 80px;
+        }
+
+        @above tablet {
+          .main {
+            padding: 0 ${`0 calc(${theme.space.default} * 1.5)`};
+          }
+        }
+        
+      `}</style>
+    </React.Fragment>
+  );
+};
+
+Blog.propTypes = {
+  posts: PropTypes.array.isRequired,
+  theme: PropTypes.object.isRequired
+};
+
+export default Blog;
